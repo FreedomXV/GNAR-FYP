9e7b45a8b7c9ef97b8164e2da751f362b96e4665
Zaki Zainudin <zaki.b.zainudin@gmail.com>
2024-09-14 11:47:56 +0800

    Add Python scripts

---
+++ Data Processing/LTA_API.py
@@ -0,0 +1,9 @@
+api_url = 'http://datamall2.mytransport.sg/ltaodataservice/PV/'
+
+endpoints = [
+    "http://datamall2.mytransport.sg/ltaodataservice/PV/ODTrain"
+    , "http://datamall2.mytransport.sg/ltaodataservice/PV/Train"
+    , "http://datamall2.mytransport.sg/ltaodataservice/TrafficFlow"
+    , "http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents"
+    , "http://datamall2.mytransport.sg/ltaodataservice/RoadWorks"
+]
---
+++ Data Processing/generate_data.py
@@ -0,0 +1,21 @@
+import os
+from keys import account_key
+
+api_url = 'http://datamall2.mytransport.sg/ltaodataservice/PV/'
+
+endpoints = [
+    "http://datamall2.mytransport.sg/ltaodataservice/PV/ODTrain"
+    , "http://datamall2.mytransport.sg/ltaodataservice/PV/Train"
+    , "http://datamall2.mytransport.sg/ltaodataservice/TrafficFlow"
+    , "http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents"
+    , "http://datamall2.mytransport.sg/ltaodataservice/RoadWorks"
+]
+api_url = 'http://datamall2.mytransport.sg/ltaodataservice/TrafficFlow'
+print(api_url)
+
+def main():
+
+    dataset_base_path = os.path.join(os.getcwd(), 'Data')
+
+    if not os.path.exists(dataset_base_path):
+        os.makedirs(dataset_base_path)
