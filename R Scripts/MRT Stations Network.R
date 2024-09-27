a8750623bed146a4f804ade982b7a7b678582d06
Zaki Zainudin <zaki.b.zainudin@gmail.com>
2024-09-27 19:35:18 +0800

    Added working directory

--- R Scripts/MRT Stations Network.R
+++ R Scripts/MRT Stations Network.R
@@ -1,7 +1,7 @@
 # R Script Testing for MRT Stations
-Sys.getenv()
-setwd("C:/Users/shygu/Documents/GitHub/GNAR-FYP")
-setwd("C:/Users/USER/Documents/GitHub/GNAR-FYP")
+dir = paste(Sys.getenv("R_USER"), "/GitHub/GNAR-FYP", sep = "")
+dir
+setwd(dir)
 
 library("GNAR")
 library("igraph")
